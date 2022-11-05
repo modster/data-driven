7da27a566922f0d885ead381df0c87b1e397a247
E. M. Greeff <unmodster@gmail.com>
2022-11-04 21:26:01 -0600

    added splinejs

---
+++ pages/Ddd.js
@@ -0,0 +1,8 @@
+//https://draft.spline.design/qUtje8JBlyrqFMLL/scene.splinecode
+import Spline from '@splinetool/react-spline'
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/NkEuk3j3PItD6H9U/scene.splinecode" />
+  )
+}
